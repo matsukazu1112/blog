5dc1e4cb23903f0cb2cd08f3820b2f9cb5c6196c
松井和也 <matsukazu19951112@gmail.com>
2019-07-16 22:02:05 +0900

    fix: DBのバージョンを変更しました

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2019_06_04_231804) do
 
-  create_table "comments", force: :cascade do |t|
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
     t.string "post"
     t.string "text"
-    t.integer "post_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
